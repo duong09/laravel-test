06040c36172826e4aec7e665a2bd9d060ae3fd44
duong09 <vduong0293@gmail.com>
2022-04-28 10:28:18 +0700

    Change layout blade and database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,34 +37,5 @@ class DatabaseSeeder extends Seeder
                 ->for($category)
                 ->create();
         }
-        // $product = Category::create([
-        //     'name' => 'Product',
-        //     'slug' => 'product'
-        // ]);
-        // $skill = Category::create([
-        //     'name' => 'Skill',
-        //     'slug' => 'skill'
-        // ]);
-        // $job = Category::create([
-        //     'name' => 'Job',
-        //     'slug' => 'job'
-        // ]);
-        // Post::create([
-        //     'user_id' => $user->id,
-        //     'category_id' => $skill->id,
-        //     'title' => 'My First Product',
-        //     'slug' => 'my-first-product',
-        //     'body' => 'Laravel is a PHP web application framework with expressive, elegant syntax.
-        //      We ve already laid the foundation — freeing you to create without sweating'
-        // ]);
-
-        // Post::create([
-        //     'user_id' => $user->id,
-        //     'category_id' => $job->id,
-        //     'title' => 'My First Job',
-        //     'slug' => 'my-first-job',
-        //     'body' => 'Laravel is a PHP web application framework with expressive, elegant syntax.
-        //      We ve already laid the foundation — freeing you to create without sweating'
-        // ]);
     }
 }
